b6e2ca2bb11d395f96df82dae12c7ab8bfb68aca
Ankita <ankitaharihar81@gmail.com>
2026-03-05 14:29:49 +0530

    Add initial implementation of Tic Tac Toe game
    - Created index.html for the game layout with a 3x3 grid.
    - Added script.js to handle game logic and player interactions.
    - Included style.css for basic styling of the game board and cells.

---
+++ tic-tac-toe/script.js
@@ -0,0 +1,18 @@
+let board = ["", "", "", "", "", "", "", "", ""];
+const cells = document.querySelectorAll(".cell");
+console.log(cells);
+let currentPlayer = "X";
+let gameActive = true;
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],    
+    [6, 7, 8],
+];
+
+cells.forEach(function(cell){
+
+});
+cell.addEventListener(cell)
+function handleCellClick(event){
+
+}
